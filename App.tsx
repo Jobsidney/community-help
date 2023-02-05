89c48ee296c1da3cace1aa083c9315aab199cafd
Jobsidney <jobsidney67@gmail.com>
2023-02-05 22:27:44 +0300

    routes setuop

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './src/screens/Intro';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import IntroPage from './src/screens/Intro';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text className='text-red-700'>Open up App.js to start working on your app!</Text>
-      <StatusBar />
-    </View>
+    <NavigationContainer>
+       <Stack.Navigator>
+        <Stack.Screen
+          name="Intro"
+          component={IntroPage}
+          options={{
+            headerShown: false,
+          }}       
+        /></Stack.Navigator>
+       
+        <StatusBar />
+      </NavigationContainer>
+  
   );
 }
 
