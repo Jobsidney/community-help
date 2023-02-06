27668bbf00d75ceea375cf5efe2a64248399fa5d
Jobsidney <jobsidney67@gmail.com>
2023-02-06 21:43:37 +0300

    category

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,6 +1,6 @@
 import { View, Text,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
-import * as Progress from 'react-native-progress';
+
 import { useNavigation, useRoute } from '@react-navigation/native'
 type Props = {}
 
@@ -14,7 +14,7 @@ const CategoryCard = ({
     <TouchableOpacity onPress={()=>navigation.navigate('SingleCharity',{
         id,imageUrl,title,description,period,days_left,Current_Donation,target_Donation,campaign_leader
     })} className='bg-white shadow-md mx-1 my-4 rounded-md p-4' style={{shadowColor:'gray'}}>
-      <Image className='w-full h-[150px] rounded-md' source={{uri:'https://i.pinimg.com/236x/f3/51/21/f35121925403eecec69bc82e573b6dbd.jpg'}}/>
+      <Image className='w-full h-[150px] rounded-md' source={{uri:`${imageUrl}`}}/>
       <Text className='font-bold text-[18px] mb-3 tracking-wider'>Help Them Smile Again</Text>
       <View className='flex-col '>
         <Text className='text-[#c5c5c5]'>Donation raised</Text>
@@ -30,11 +30,3 @@ const CategoryCard = ({
 
 export default CategoryCard
 
-// <Progress.Bar progress={0.7} width={320} color={'#da5221'} unfilledColor={'#F4F4F6'} borderColor="white" />
-// <View className='flex-row justify-between space-y-4 items-center'>
-//   <View className='flex-row'>
-//       <Text className='font-bold'>$52,700</Text>
-//       <Text className='text-[#A4A4A4]'>/70,000</Text>
-//   </View>
-//   <Text className='font-bold'>82%</Text>
-// </View>
