347c4d1145c4b57c310748bd2035f211af8e9940
Jobsidney <jobsidney67@gmail.com>
2023-02-06 16:42:36 +0300

    drawer menu set

---
+++ src/components/DrawerButton.tsx
@@ -0,0 +1,17 @@
+import { View, Text,Image,TouchableOpacity,TouchableWithoutFeedback } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons'; 
+import { useNavigation } from '@react-navigation/native';
+type Props = {}
+
+const DrawerButton = ({navigation}) => {
+    
+    return(
+        <TouchableOpacity onPress={()=>navigation.openDrawer()}>
+            <Ionicons name="reorder-three" size={34} color="black" />
+        </TouchableOpacity>
+    )
+}
+
+export default DrawerButton
