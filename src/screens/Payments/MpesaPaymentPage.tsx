76387d2a2fa00120c2e9c71742171e060bcf0368
Jobsidney <wmuriithitabitha@gmail.com>
2023-02-09 21:46:55 +0300

    mpesa page

---
+++ src/screens/Payments/MpesaPaymentPage.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const MpesaPaymentPage = (props: Props) => {
+  return (
+    <View>
+      <Text>MpesaPaymentPage</Text>
+    </View>
+  )
+}
+
+export default MpesaPaymentPage
+
+const styles = StyleSheet.create({})
