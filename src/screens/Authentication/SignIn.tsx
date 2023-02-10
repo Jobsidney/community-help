385c352832341a96833ab52106182f8dadde4b5f
Jobsidney <jobsidney67@gmail.com>
2023-02-10 21:58:09 +0300

    sign in

--- src/screens/Authentication/SignIn.tsx
+++ src/screens/Authentication/SignIn.tsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View,TouchableOpacity,TextInput } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity,TextInput,Keyboard,TouchableWithoutFeedback } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { MaterialIcons } from '@expo/vector-icons'; 
 import { useNavigation } from '@react-navigation/native';
 type Props = {}
 
-const SignUp = (props: Props) => {
+const SignIn = (props: Props) => {
     //jigigk
     const navigation=useNavigation()
+    Keyboard.dismiss()
   return (
     <View className='bg-white flex-1 '>
         <SafeAreaView className='px-4 '>
@@ -15,10 +16,11 @@ const SignUp = (props: Props) => {
                 <TouchableOpacity onPress={()=>navigation.goBack()}>
                     <MaterialIcons name="arrow-back" size={24} color="black" />
                 </TouchableOpacity>
+                <TouchableOpacity onPress={()=>navigation.navigate("Register")}><Text className='font-bold tracking-widest underline '>Register</Text></TouchableOpacity>
 
             </View>
         </SafeAreaView>
-        <View className='px-4 space-y-12'>
+        <View   className='px-4 space-y-12'>
             <View>
                 <Text className='font-bold text-2xl tracking-wider text-[#da5221] '>Sign In</Text>
                 <Text className='tracking-wide'>Refugees International is committed to saving the lives and protecting the rights Refugees International is committed to saving the lives and protecting the rights</Text>
@@ -28,7 +30,7 @@ const SignUp = (props: Props) => {
                 <TextInput placeholder='Password' keyboardType='email-address' className='bg-[#E6E5E6] rounded-3xl pl-6 py-2'/>
                 <TouchableOpacity><Text className='underline font-semibold text-right tracking-wider'>Forgot password?</Text></TouchableOpacity>
             </View>
-            <TouchableOpacity className='bg-[#da5221] items-center py-3 rounded-3xl'>
+            <TouchableOpacity onPress={()=> Keyboard.dismiss()} className='bg-[#da5221] items-center py-3 rounded-3xl'>
                 <Text className='text-white text-[16px] tracking-widest'>Sign In</Text>
             </TouchableOpacity>
 
@@ -37,6 +39,6 @@ const SignUp = (props: Props) => {
   )
 }
 
-export default SignUp
+export default SignIn
 
 const styles = StyleSheet.create({})
