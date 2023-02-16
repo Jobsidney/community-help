e46714bb47ac33ee9868e610709a9099bbbc29b6
Jobsidney <jobsidney67@gmail.com>
2023-02-16 22:57:56 +0300

    card change

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,6 +1,6 @@
 import { View, Text,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
-
+import { Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native'
 type Props = {}
 
@@ -13,17 +13,32 @@ const CategoryCard = ({
   return (
     <TouchableOpacity onPress={()=>navigation.navigate('SingleCharity',{
         id,imageUrl,title,description,period,days_left,Current_Donation,target_Donation,campaign_leader
-    })} className='bg-white shadow-md mx-1 my-4 rounded-md p-4' style={{shadowColor:'gray'}}>
-      <Image className='w-full h-[150px] rounded-md' source={{uri:`${imageUrl}`}}/>
-      <Text className='font-bold text-[18px] mb-3 tracking-wider'>Help Them Smile Again</Text>
-      <View className='flex-col '>
-        <Text className='text-[#c5c5c5]'>Donation raised</Text>
-        <View className='flex-row justify-between space-y-2 items-center'>
-            <Text className='font-bold text-[#da5221]  text-[18px]'>$52,700</Text>
-            <Text className='font-bold text-[#c5c5c5] text-[14px] '>9 Days Left</Text>
-        </View>
-        
+    })} className='bg-white relative  shadow-md   w-[250px] h-[250px] mr-4 rounded-md ' style={{shadowColor:'gray'}}>
+      <View className='absolute z-50 bg-green-900 rounded-md ml-3 px-2 py-1  flex-row items-center -mt-4 space-x-1'><Ionicons name="location-outline" size={12} color="white" /><Text className='text-white font-bold tracking-widest text-xs'>India</Text></View>
+      <Image className='w-full h-[80%] rounded-t-md' source={{uri:`${imageUrl}`}}/>
+      <View className='absolute bottom-[20%] px-1 w-full'>
+        <Text className='font-bold text-[20px] text-white mb-3 tracking-wider'>Help Them Smile Again</Text>
+
       </View>
+      <View className='flex-col w-full space-y-1 pl-1 '>
+          {/* <Text className='text-[#c5c5c5]'>Donation raised</Text> */}
+          <Text className='font-light text-black  text-xs'>$52,700 raised from $70,000 total</Text>
+          <View className='flex-row   items-center space-x-4 w-full'>
+            <View className='flex-row items-center space-x-1'>
+              <Ionicons name="md-people-outline" size={12} color="black" />
+                <Text className=' text-black text-xs '>250 people</Text>
+            </View>
+
+            <View className='flex-row items-center'>
+              <Ionicons name="ios-time-outline" size={12} color="black" />
+              <Text className=' text-black text-xs '>9 days Left</Text>
+            </View>
+              
+              
+          </View>
+          
+        </View>
+      
     </TouchableOpacity>
   )
 }
