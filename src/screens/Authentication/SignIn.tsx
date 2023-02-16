24ff42193c2e76be58662aec4584f666b6114184
Jobsidney <jobsidney67@gmail.com>
2023-02-16 22:58:36 +0300

    single page

--- src/screens/Authentication/SignIn.tsx
+++ src/screens/Authentication/SignIn.tsx
@@ -1,16 +1,26 @@
 import { StyleSheet, Text, View,TouchableOpacity,TextInput,Keyboard,TouchableWithoutFeedback } from 'react-native'
-import React from 'react'
+import React,{useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { MaterialIcons } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons'; 
 import { useNavigation } from '@react-navigation/native';
+import AppLoader from '../../components/AppLoader';
 type Props = {}
 
 const SignIn = (props: Props) => {
     //jigigk
     const navigation=useNavigation()
-    Keyboard.dismiss()
+    const[password,setPassword]=useState('')
+    const [secure, setSecure] = useState(false);
+    const [loadingPending,setLoadingPeddings] = useState(false)
+    function handleChange(data) {
+        setPassword(data)
+
+     }
+
   return (
     <View className='bg-white flex-1 '>
+        {loadingPending? <AppLoader/>: null}
         <SafeAreaView className='px-4 '>
         <View className='h-[50px] flex-row items-center justify-between'>
                 <TouchableOpacity onPress={()=>navigation.goBack()}>
@@ -27,10 +37,25 @@ const SignIn = (props: Props) => {
             </View>
             <View className='space-y-3'>
                 <TextInput placeholder='Email' keyboardType='email-address' className='bg-[#E6E5E6] rounded-3xl pl-6 py-2'/>
-                <TextInput placeholder='Password' keyboardType='email-address' className='bg-[#E6E5E6] rounded-3xl pl-6 py-2'/>
+                <View className='bg-[#E6E5E6] rounded-3xl pl-6 py-2 flex-row items-center'>
+                    <TextInput
+                    onChangeText={(pass) => handleChange(pass)}
+                    placeholder="Password"  
+                    className='flex-1 '
+                    keyboardType='email-address'
+                    secureTextEntry={secure} 
+                    />
+                    <FontAwesome style={{ paddingRight: 15, }}
+                    name={secure ? "eye" : 'eye-slash'}
+                    size={20} color='gray' 
+                    onPress={() => setSecure(!secure)} />
+                </View>
+                
                 <TouchableOpacity><Text className='underline font-semibold text-right tracking-wider'>Forgot password?</Text></TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={()=> Keyboard.dismiss()} className='bg-[#da5221] items-center py-3 rounded-3xl'>
+            <TouchableOpacity onPress={()=> {
+                setLoadingPeddings(true)
+                Keyboard.dismiss()}} className='bg-[#da5221] items-center py-3 rounded-3xl'>
                 <Text className='text-white text-[16px] tracking-widest'>Sign In</Text>
             </TouchableOpacity>
 
