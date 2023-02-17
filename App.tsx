8e65d3e37130fe95dda9b21100476f451d3a6da1
Jobsidney <jobsidney67@gmail.com>
2023-02-17 22:58:45 +0300

    added bottom tab

--- App.tsx
+++ App.tsx
@@ -12,73 +12,75 @@ import SignUp from './src/screens/Authentication/SignIn';
 import Welcome from './src/screens/Authentication/Welcome';
 import SignIn from './src/screens/Authentication/SignIn';
 import Register from './src/screens/Authentication/Register';
+import MainContainer from './src/navigation/MainContainer';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-       <Stack.Navigator>
-        <Stack.Screen
-          name="Intro"
-          component={IntroPage}
-          options={{
-            headerShown: false,
-          }}       
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: false,
-          }}       
-        />
-        <Stack.Screen
-          name="SingleCharity"
-          component={SingleCharityScreen}
-          options={{
-            headerShown: false,
-          }}       
-        />
-         <Stack.Screen
-          name="PaymentOption"
-          component={PaymentOption}
-          options={{
-            headerShown: false,
-          }}       
-        />
-        <Stack.Screen
-          name="MpesaPaymentOption"
-          component={MpesaPaymentPage}
-          options={{
-            headerShown: false,
-          }}       
-        />
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{
-            headerShown: false,
-          }}       
-        />
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{
-            headerShown: false,
-          }}       
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{
-            headerShown: false,
-          }}       
-        />
-        </Stack.Navigator>
+    <MainContainer/>
+    // <NavigationContainer>
+    //    <Stack.Navigator>
+    //     <Stack.Screen
+    //       name="Intro"
+    //       component={IntroPage}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //     <Stack.Screen
+    //       name="Home"
+    //       component={Home}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //     <Stack.Screen
+    //       name="SingleCharity"
+    //       component={SingleCharityScreen}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //      <Stack.Screen
+    //       name="PaymentOption"
+    //       component={PaymentOption}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //     <Stack.Screen
+    //       name="MpesaPaymentOption"
+    //       component={MpesaPaymentPage}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //     <Stack.Screen
+    //       name="Welcome"
+    //       component={Welcome}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //     <Stack.Screen
+    //       name="SignIn"
+    //       component={SignIn}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //     <Stack.Screen
+    //       name="Register"
+    //       component={Register}
+    //       options={{
+    //         headerShown: false,
+    //       }}       
+    //     />
+    //     </Stack.Navigator>
        
-        <StatusBar />
-      </NavigationContainer>
+    //     <StatusBar />
+    //   </NavigationContainer>
   
   );
 }
