593772f18287248a663be03396c674a932d0b379
Jobsidney <jobsidney67@gmail.com>
2023-02-18 01:32:45 +0300

    categ

--- src/components/CategoryIcon.tsx
+++ src/components/CategoryIcon.tsx
@@ -8,11 +8,11 @@ type Props = {}
 const CategoryIcon = ({action,icon,word}) => {
 
     return(
-    <View className='bg-[#F4F4F6] flex-row m-1 mr-3 items-center space-x-1 px-3 py-1  rounded-lg'>
-        <TouchableOpacity className='flex-row  items-center space-x-1 px-2 py-1  rounded-lg'>
-            {icon}
-            <Text className='text-black font-semibold'>{word}</Text>
-        </TouchableOpacity>     
+    <View className='flex-col m-1 mr-3 items-center space-x-1  p-1 justify-center  rounded-lg'>
+        <TouchableOpacity className='flex-col bg-[#addacd] w-[50px] h-[50px]  items-center justify-center  rounded-full'>
+            {icon}    
+        </TouchableOpacity> 
+        <Text className='text-xs '>{word}</Text>    
 
     </View>
     )
