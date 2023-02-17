805cc30323d64f9a6e4377633881f1dcf45b7890
Jobsidney <jobsidney67@gmail.com>
2023-02-18 01:32:06 +0300

    restructure

--- src/screens/Home/LandingScreen.jsx
+++ src/screens/Home/LandingScreen.jsx
@@ -8,6 +8,7 @@ import { SearchBar } from 'react-native-elements';
 import CategoryIcon from '../../components/CategoryIcon';
 import { Feather } from '@expo/vector-icons'; 
 import { FontAwesome } from '@expo/vector-icons'; 
+import { FontAwesome5 } from '@expo/vector-icons'; 
 import CategoryCard from '../../components/CategoryCard';
 import FeaturedRow from '../../components/FeatureRow';
 type Props = {}
@@ -33,48 +34,53 @@ const LandingScreen = (props: Props) => {
     }
     
   return (
-    <View className='flex-1 bg-[#FAFAFA]'>
-      <View className='h-[200px] bg-[#da5221] pt-8 px-2 rounded-b-2xl'>
-      <View className='flex-row w-full items-center justify-between'>
-        <DrawerButton navigation={navigation}/>
-          <View>
-              {/* <Text className='text-[16px] tracking-widest'>Hello Good People</Text> */}
-          </View>
-          <TouchableWithoutFeedback onPress={()=>{}}>
-          <Ionicons name="notifications" size={24} color="white" />
+    <View className='flex-1 bg-[white]'>
+      <SafeAreaView className='flex-1 pt-4'>
+      <View className='mb-4'>
+      <View className='flex-row mx-4  items-center justify-between'>
+
+        {/* start of search */}
+        <View className=' flex-row items-center space-x-2 bg-[#e7e8ec] pl-2 rounded-3xl w-[80%]'>
+          <View className=' rounded-full items-center justify-center'><Ionicons name="search" size={20} color="gray" /></View> 
+          <TextInput className='flex-1 h-[37px]' placeholder='Search for causes'/>
+          
+        </View>
+
+          <TouchableOpacity onPress={()=>{console.log('hello');}} className='rounded-full   p-1 border border-gray-300'>
+            <Ionicons name="notifications" size={20} color="black" />
               
-          </TouchableWithoutFeedback>
+          </TouchableOpacity>
       </View>
-      <View className=' pt-4'>
+      {/* <View className=' pt-4'>
     
       <Image className='w-[40px] h-[40px]rounded-full' source={{uri:'https://i.pinimg.com/236x/f3/51/21/f35121925403eecec69bc82e573b6dbd.jpg'}}/>
       
         
+      </View> */}
       </View>
-      </View>
-      <View className='-mt-[25px] mx-5 flex-row items-center space-x-2 bg-[#f5eee7] pl-2 rounded-3xl'>
-        
-        <TextInput className='flex-1 h-[45px]' placeholder='Search for causes'/>
-        <View className='bg-black h-[45px] w-[45px] rounded-full items-center justify-center'><Ionicons name="search" size={20} color="white" /></View>
-        
-      </View>
-    <SafeAreaView className='  flex-1 px-4   space-y-4 '>
+      
+    <View className='  flex-1 px-4   space-y-4 '>
       
 
       <ScrollView  showsVerticalScrollIndicator={false} className='space-y-2'>
-      {/* <View className=' bg-[#da5221] rounded-lg h-[100px] px-2 items-center space-x-3  flex-row'>
-            <View className='bg-white rounded-md'>
-                <Ionicons  name="heart-sharp" size={44} color="#da5221" />
-            </View>
-            
-           
-            <Text className='text-white/80 flex-1  font-semibold text-lg '>No one has ever become poor by giving</Text>
-         
-     
-      </View> */}
+      <View className='flex-row items-center rounded-2xl border border-gray-300 basis-[100%]'> 
+        <View className=' basis-[70%] bg-[#1F2D36] rounded-2xl flex-row items-center space-x-3   -mr-2 h-[90px] pl-6'>
+          <View>
+            <FontAwesome5 name="money-bill-wave" size={24} color="white"  />
+          </View>
+          <View className='space-y-1'>
+            <Text className='text-white/60'>Your donation level</Text>
+            <Text className='text-white text-2xl font-bold'>$ 340,898</Text>
+          </View>
+        </View>
+        <View className='basis-[30%] h-full border-l-0 flex-col items-center justify-center  rounded-r-2xl'>
+          <Text className='font-bold'>Maximized?</Text>
+          <Ionicons name="checkmark-circle" size={24} color="black" />
+        </View>
+      </View>
 
-      {/* <View className='flex-col  sticky top-0 z-44'>
-        <Text className='font-bold text-lg tracking-normal mb-3'>Categories</Text>
+      <View className='flex-col  sticky top-0 z-44'>
+        {/* <Text className='font-bold text-lg tracking-normal mb-3'>Categories</Text> */}
         <ScrollView horizontal showsHorizontalScrollIndicator={false} className='flex-row '>
             <CategoryIcon icon={<Feather name="pen-tool" size={16} color="#da5221"  />}word="Humanity" />
             <CategoryIcon icon={<FontAwesome name="stethoscope" size={16} color="#da5221"/>} word="Health"/>
@@ -86,13 +92,13 @@ const LandingScreen = (props: Props) => {
             <CategoryIcon icon={<Feather name="pen-tool" size={16} color="black" />} word="Education"/>
 
         </ScrollView>
-      </View> */}
+      </View>
       <View className=' space-y-2'>
-        <FeaturedRow title={'Need to Help'} description={'View All'}/>
+        <FeaturedRow title={'Urgent Fundraising'} description={'View All'}/>
         <FeaturedRow title={'Favourite'} description={'View All'}/>
       </View></ScrollView>
 
-    </SafeAreaView></View>
+    </View></SafeAreaView></View>
   )
 }
 //hey
