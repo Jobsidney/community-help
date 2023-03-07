df714fbb4cc18194af9117600724cb831942b2c4
Jobsidney <Jobsidney67@gmail.com>
2023-03-07 22:28:05 +0300

    change on category icon

--- src/components/CategoryIcon.tsx
+++ src/components/CategoryIcon.tsx
@@ -5,11 +5,13 @@ import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 type Props = {}
 
-const CategoryIcon = ({action,icon,word}) => {
-
+const CategoryIcon = ({action,icon,word,data}) => {
+    const navigation=useNavigation()
     return(
     <View className='flex-col m-1 mr-3 items-center space-x-1  p-1 justify-center  rounded-lg'>
-        <TouchableOpacity className='flex-col bg-[#addacd] w-[50px] h-[50px]  items-center justify-center  rounded-full'>
+        <TouchableOpacity onPress={()=>{navigation.navigate('SingleCategory',{
+        word,data
+    })}} className='flex-col bg-[#addacd] w-[50px] h-[50px]  items-center justify-center  rounded-full'>
             {icon}    
         </TouchableOpacity> 
         <Text className='text-xs '>{word}</Text>    
