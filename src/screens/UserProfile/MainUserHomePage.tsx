de75f84932af5da128eaa0658f09e30442ad1e22
Jobsidney <Jobsidney67@gmail.com>
2023-03-08 21:34:39 +0300

    home user

---
+++ src/screens/UserProfile/MainUserHomePage.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import WavyBackground from "react-native-wavy-background";
+type Props = {}
+
+const MainUserHomePage = (props: Props) => {
+  return (
+    <View className='flex-1 bg-red-900'>
+     
+    </View>
+  )
+}
+
+export default MainUserHomePage
